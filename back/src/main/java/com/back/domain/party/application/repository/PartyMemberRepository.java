736af7c8383a6185805a92897b65e1065bdf59ca
jjun012 <duwns2057@naver.com>
2026-08-31 10:48:48 +0900

    fix: 지원자 목록 조회 N+1 방지

--- back/src/main/java/com/back/domain/party/application/repository/PartyMemberRepository.java
+++ back/src/main/java/com/back/domain/party/application/repository/PartyMemberRepository.java
@@ -3,6 +3,7 @@ package com.back.domain.party.application.repository;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.party.application.entity.PartyMember;
 import com.back.domain.party.party.entity.Party;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,5 +14,6 @@ public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> 
 
     Optional<PartyMember> findByIdAndParty(long id, Party party);
 
+    @EntityGraph(attributePaths = {"member", "position"})
     List<PartyMember> findAllByParty(Party party);
 }
