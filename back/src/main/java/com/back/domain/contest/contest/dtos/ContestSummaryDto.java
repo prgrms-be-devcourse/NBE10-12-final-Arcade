239f87d38d2f0435cb25be28576213b5d706671c
leesm-225 <lshine12393@gmail.com>
2026-09-01 12:49:11 +0900

    chore:contest,party 연결 testcode 추가

--- back/src/main/java/com/back/domain/contest/contest/dtos/ContestSummaryDto.java
+++ back/src/main/java/com/back/domain/contest/contest/dtos/ContestSummaryDto.java
@@ -2,7 +2,7 @@ package com.back.domain.contest.contest.dtos;
 
 import com.back.domain.contest.contest.entity.Contest;
 
-public record ContestSummaryDto(
+public record ContestSummaryDto( //party에 값 내려주기 위한 dto
         long id,
         String title
 ) {
