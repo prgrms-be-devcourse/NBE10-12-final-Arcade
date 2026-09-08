28527fc0bbedd6f6ed7438ec52ee3e557fd84ef0
leesm-225 <lshine12393@gmail.com>
2026-09-08 17:41:08 +0900

    refactor: 깃허브 리포 매서드 복구

--- back/src/main/java/com/back/domain/party/github/repository/GithubInstallationRepositoryRepository.java
+++ back/src/main/java/com/back/domain/party/github/repository/GithubInstallationRepositoryRepository.java
@@ -28,4 +28,5 @@ public interface GithubInstallationRepositoryRepository extends JpaRepository<Gi
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<GithubInstallationRepository> findById(Long id);
     List<GithubInstallationRepository> findAllByInstallationInstallationId(Long installationId);
+    List<GithubInstallationRepository> findAllByInstallationInstallationIdAndStatus(Long installationId, GithubInstallationRepositoryStatus status);
 }
