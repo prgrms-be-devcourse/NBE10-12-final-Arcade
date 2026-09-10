4dcb9b0dd114f2e7975f30dfbc92c3cdfcd8d255
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-09-10 16:37:53 +0900

    front: 개인 TODO 의 담당자 안내 문구 제거

--- front/app/mypage/todo/[id]/page.tsx
+++ front/app/mypage/todo/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { SoloSpaceLoader } from '@/components/mypage/SoloSpaceLoader';
 import { BackLink } from '@/components/ui/BackLink';
-import { MOCK_CURRENT_USER_ID, MOCK_USER_SUMMARIES } from '@/lib/mock';
 
 export default async function SoloTodoPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -9,7 +8,7 @@ export default async function SoloTodoPage({ params }: { params: Promise<{ id: s
     <main>
       <div className="board-wrap container">
         <BackLink href="/mypage?tab=todo" label="개인 TODO 목록으로" />
-        <SoloSpaceLoader id={id} ownerName={MOCK_USER_SUMMARIES[MOCK_CURRENT_USER_ID].name} />
+        <SoloSpaceLoader id={id} />
       </div>
     </main>
   );
