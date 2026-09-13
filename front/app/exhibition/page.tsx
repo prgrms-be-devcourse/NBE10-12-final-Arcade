6116f361756bf9ab452d60dd42449abfa73cc772
EasyH <skysea001010@gmail.com>
2026-09-13 15:54:53 +0900

    front: 전시관의 전시 등록 버튼 제거

--- front/app/exhibition/page.tsx
+++ front/app/exhibition/page.tsx
@@ -1,5 +1,4 @@
 import { ExhibitionBoard } from '@/components/exhibition/ExhibitionBoard';
-import { LinkButton } from '@/components/ui/Button';
 import { SectionHead } from '@/components/ui/SectionHead';
 import { fetchExhibitions } from '@/lib/api';
 
@@ -12,7 +11,6 @@ export default async function ExhibitionPage() {
         <SectionHead
           title="전시관"
           description="크루온에서 완료된 프로젝트를 만나보세요. 카드를 클릭하면 상세 페이지로, 만든 사람을 클릭하면 프로필로 이동합니다."
-          action={<LinkButton href="/exhibition/create">전시 등록</LinkButton>}
         />
         <ExhibitionBoard projects={projects} />
       </div>
